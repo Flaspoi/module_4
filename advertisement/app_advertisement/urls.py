446bf75a5da29687f3a499023866e45d04ddc663
Flaspoi <khismatovrs@gmail.com>
2023-07-17 13:36:36 +0300

    les5

--- advertisement/app_advertisement/urls.py
+++ advertisement/app_advertisement/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement_post, register, login, profile, exit
+
 
 
 urlpatterns = [
     path("", index, name="main-page"),
     path("top-sellers/", top_sellers, name="top-sellers"),
+    path("advertisement-post/", advertisement_post, name="advertisement-post"),
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
+    path("profile/", profile, name="profile"),
+    path("exit/", exit, name="exit"),
 ]
--- advertisement/app_advertisement/views.py
+++ advertisement/app_advertisement/views.py
@@ -7,3 +7,18 @@ def index(request):
 
 def top_sellers(request):
     return render(request, "top-sellers.html")
+
+def advertisement_post(request):
+    return render(request, "advertisement-post")
+
+def register(request):
+    return render(request, "register")
+
+def login(request):
+    return render(request, "login")
+
+def profile(request):
+    return render(request, "profile")
+
+def exit(request):
+    return render(request, "exit")
