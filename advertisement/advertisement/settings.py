66cc80d268f80d1b9a40911622867fcff6a5c7d9
Flaspoi <khismatovrs@gmail.com>
2023-07-31 13:02:06 +0300

    les7

--- advertisement/advertisement/settings.py
+++ advertisement/advertisement/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'advertisement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'flaspoi',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT':  5432
+
     }
 }
 
--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField(
+        verbose_name="Заголовок",
+        max_length=100,
+        )
+    description= models.TextField(
+        verbose_name="Описание",
+    )
+    price = models.DecimalField(
+        verbose_name="Цена",
+        max_digits=10,
+        decimal_places=2
+    )
+    auction = models.BooleanField(
+        verbose_name="Торг",
+        default=False,
+        help_text="Отметьте, если торг уместен!"
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата добавления",
+        auto_now_add = True,
+    ),
+    updated_at = models.DateTimeField(
+        verbose_name="Дата редактирования",
+        auto_now=True,
+    )
