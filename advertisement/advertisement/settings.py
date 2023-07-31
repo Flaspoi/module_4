3e99a11f6b3f2ea6f97794924fdf9512decc3c38
Flaspoi <khismatovrs@gmail.com>
2023-07-31 21:09:51 +0300

    trash

--- advertisement/advertisement/settings.py
+++ advertisement/advertisement/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'advertisement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'flaspoi',
         'PASSWORD': '',
--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 class Advertisement(models.Model):
@@ -20,10 +22,28 @@ class Advertisement(models.Model):
         help_text="Отметьте, если торг уместен!"
     )
     created_at = models.DateTimeField(
-        verbose_name="Дата добавления",
-        auto_now_add = True,
-    ),
+        auto_now_add=True,
+        verbose_name="Дата публикации"
+    )
     updated_at = models.DateTimeField(
         verbose_name="Дата редактирования",
         auto_now=True,
     )
+    
+    def __str__(self):
+        return f"id = {self.id} title = {self.title} price = {self.price}"
+
+    class Meta:
+        db_table = "advertisement"
+    
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    
