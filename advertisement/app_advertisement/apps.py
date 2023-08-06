c9fb4a0a04c8e5974d7ebaa8d1c8ff01a4c4995f
Flaspoi <khismatovrs@gmail.com>
2023-08-06 17:29:08 +0300

    les8_hw

--- advertisement/app_advertisement/apps.py
+++ advertisement/app_advertisement/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class AppAdvertisementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_advertisement'
+    name = 'app_advertisements'
+    verbose_name = 'Обьявление'
+
--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -45,5 +45,14 @@ class Advertisement(models.Model):
                 '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', updated_time
+            )
+        # else:
+        
 
     
